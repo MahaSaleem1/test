a2a57f1787a7b71aa00a47ada1116ed72943c079
Saleem <maha.saleem@accenture.com>
2019-10-25 14:27:16 -0500

    second commit!!

---
+++ buildd/VlocityUITemplate/Test-VBT/Test-VBT.js
@@ -0,0 +1,6 @@
+vlocity.cardframework.registerModule
+    .controller('testCtrl',
+                ['$scope', '$timeout', '$interval', '$filter',
+                    function($scope, $timeout, $interval, $filter) {
+                $scope.bpTree.response;        
+}]);
